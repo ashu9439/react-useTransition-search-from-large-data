6c281067ca4d993c0600fe77d2ca43b6297ff641
ashu9439 <46566670+ashu9439@users.noreply.github.com>
2023-04-30 14:48:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import UseTransition from "./useTransition/UseTransition";
 import WithoutUseTransition from "./useTransition/WithoutUseTransition";
 
 const App = () => {
   const [withHook, setWithHook] = useState(true);
+  const [isPending, startTransition] = useTransition();
+
+  const toggle = () => {
+    // NOTE: Prioritize input and output of user input by delaying state updates.
+    startTransition(() => {
+      setWithHook(!withHook);
+    });
+  };
   return (
     <>
       with useTransition():
-      <input
-        type="checkbox"
-        checked={withHook}
-        onClick={() => setWithHook(!withHook)}
-      />
-      {withHook ? <UseTransition /> : <WithoutUseTransition />}
+      <input type="checkbox" checked={withHook} onClick={toggle} />
+      <div>
+        {isPending ? (
+          "Loading"
+        ) : withHook ? (
+          <UseTransition />
+        ) : (
+          <WithoutUseTransition />
+        )}
+      </div>
     </>
   );
 };
 
 export default App;
+
